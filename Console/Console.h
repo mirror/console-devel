32703fc637a3190deeef9d948d46c126f39e5757
vds <unknown>
2009-08-27 05:30:55 +0300

    Add -reuse-tab and -new-tab command flag to override the corresponding global settings.

--- Console/Console.h
+++ Console/Console.h
@@ -6,8 +6,10 @@ extern shared_ptr<ImageHandler>		g_imageHandler;
 
 //////////////////////////////////////////////////////////////////////////////
 /// Flags received from command line
-#define CLF_REUSE_PREV_INSTANCE			0x0001	///< if another instance of console is already running open tabs in it even if "allow multiple instances" is enabled
-#define CLF_FORCE_NEW_INSTANCE			0x0002	///< start another instance of console even if "allow multiple instances" is disabled
+extern const unsigned int CLF_REUSE_PREV_INSTANCE;		// if another instance of console is already running open tabs in it even if "allow multiple instances" is enabled
+extern const unsigned int CLF_FORCE_NEW_INSTANCE;		// start another instance of console even if "allow multiple instances" is disabled
+extern const unsigned int CLF_REUSE_PREV_TAB;			// ...
+extern const unsigned int CLF_FORCE_NEW_TAB;			// create another tab in console even if "reuse tab" is on
 
 //////////////////////////////////////////////////////////////////////////////
 
