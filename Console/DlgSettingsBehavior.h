d28059bbcc3a870db7c4437e49036bc31ba422c1
Vivian De Smedt <vivian@vdesmedt.com>
2012-02-04 20:16:12 +0100

    merge b148 changes

--- Console/DlgSettingsBehavior.h
+++ Console/DlgSettingsBehavior.h
@@ -21,17 +21,17 @@ class DlgSettingsBehavior
 		DlgSettingsBehavior(CComPtr<IXMLDOMElement>& pOptionsRoot);
 
 		BEGIN_DDX_MAP(DlgSettingsBehavior)
-			DDX_CHECK(IDC_CHECK_COPY_ON_SELECT, m_nCopyOnSelect)
-			DDX_CHECK(IDC_CHECK_CLEAR_ON_COPY, m_nClearOnCopy)
-			DDX_CHECK(IDC_CHECK_NO_WRAP, m_nNoWrap)
-			DDX_CHECK(IDC_CHECK_TRIM_SPACES, m_nTrimSpaces)
+			DDX_CHECK(IDC_CHECK_COPY_ON_SELECT, m_behaviorSettings.copyPasteSettings.bCopyOnSelect)
+			DDX_CHECK(IDC_CHECK_CLEAR_ON_COPY, m_behaviorSettings.copyPasteSettings.bClearOnCopy)
+			DDX_CHECK(IDC_CHECK_SENSITIVE_COPY, m_behaviorSettings.copyPasteSettings.bSensitiveCopy)
+			DDX_CHECK(IDC_CHECK_NO_WRAP, m_behaviorSettings.copyPasteSettings.bNoWrap)
+			DDX_CHECK(IDC_CHECK_TRIM_SPACES, m_behaviorSettings.copyPasteSettings.bTrimSpaces)
 			DDX_RADIO(IDC_RADIO_COPY_NEWLINE_CHAR, m_nCopyNewlineChar)
 			DDX_RADIO(IDC_PAGE_SCROLL, m_nScrollPageType)
 			DDX_UINT(IDC_SCROLL_PAGE_ROWS, m_behaviorSettings.scrollSettings.dwPageScrollRows)
-			DDX_CHECK(IDC_CHECK_FLASH_TAB, m_nFlashInactiveTab)
+			DDX_CHECK(IDC_CHECK_FLASH_TAB, m_bFlashInactiveTab)
 			DDX_UINT(IDC_TAB_FLASHES, m_behaviorSettings.tabHighlightSettings.dwFlashes)
-			DDX_CHECK(IDC_CHECK_LEAVE_HIGHLIGHTED, m_nLeaveHighlighted)
-
+			DDX_CHECK(IDC_CHECK_LEAVE_HIGHLIGHTED, m_behaviorSettings.tabHighlightSettings.bStayHighlighted)
 			// vds: >>
 			DDX_CHECK(IDC_ALLOW_MULTIPLE_INSTANCES, m_nAllowMultipleInstances)
 			DDX_CHECK(IDC_REUSE_TAB, m_nReuseTab)
@@ -82,18 +82,11 @@ class DlgSettingsBehavior
 
 		BehaviorSettings	m_behaviorSettings;
 
-		int					m_nCopyOnSelect;
-		int					m_nClearOnCopy;
-		int					m_nNoWrap;
-		int					m_nTrimSpaces;
-
 		int					m_nCopyNewlineChar;
 
 		int					m_nScrollPageType;
 
-		int					m_nFlashInactiveTab;
-		int					m_nLeaveHighlighted;
-
+		bool				m_bFlashInactiveTab;
 		// vds: >>
 		int					m_nAllowMultipleInstances;
 		int					m_nReuseTab;
